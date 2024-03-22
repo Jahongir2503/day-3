da5d4bd2bcbe2e79416402f6e3b3a61979238c96
mirzo <jojo.isiomidinov@gmail.com>
2024-03-22 10:18:51 +0500

    start

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -2,15 +2,15 @@
 
 use PHPUnit\Framework\TestCase;
 use Collects\Collect;
+
 require __DIR__ . '\..\src\Collect.php';
 
 class CollectTest extends TestCase
 {
     public function testCount()
     {
-
-        $collect = new Collect([13,17]);
-        $this->assertSame(2, $collect->count());
+        $collect = new Collect([13, 17, 12]);
+        $this->assertSame(3, $collect->count());
     }
 
 }
